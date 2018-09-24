a9579d67c3e084accbb5a0b80d734a9c9b67d568
anttinym <nyman.antti@gmail.com>
2018-09-24 15:05:11 +0300

    added relevant icons to tabs and a settings button for shard preferences

--- app/src/main/java/com/penys/fi/movemaster95/HomeFragment.kt
+++ app/src/main/java/com/penys/fi/movemaster95/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 
 class HomeFragment : Fragment() {
 
@@ -20,7 +21,7 @@ class HomeFragment : Fragment() {
 
 
         val view = inflater!!.inflate(R.layout.home_layout, container, false)
-
+        val settingsButton = view?.findViewById<ImageView>(R.id.settings_button)
 
 
         return view
--- app/src/main/java/com/penys/fi/movemaster95/StatsFragment.kt
+++ app/src/main/java/com/penys/fi/movemaster95/StatsFragment.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 class StatsFragment : Fragment() {
 
@@ -12,12 +13,12 @@ class StatsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater!!.inflate(R.layout.stats_layout,container,false)
+        val settingsButton = view?.findViewById<ImageView>(R.id.settings_button)
 
         return view
 
