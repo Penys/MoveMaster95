613c5316d55279b800111b3e5d2094fd632816ea
anttinym <nyman.antti@gmail.com>
2018-10-05 14:10:53 +0300

    removed redundant double expression from step count

--- app/src/main/java/com/penys/fi/movemaster95/HomeFragment.kt
+++ app/src/main/java/com/penys/fi/movemaster95/HomeFragment.kt
@@ -50,8 +50,8 @@ class HomeFragment : Fragment(), SensorEventListener {
             val sIntent = Intent()
             sIntent.action = Intent.ACTION_SEND
             sIntent.type = "text/plain"
-            sIntent.putExtra(Intent.EXTRA_TEXT, st)
-            sIntent.putExtra(Intent.EXTRA_SUBJECT, "Look how much have I walked today!")
+            sIntent.putExtra(Intent.EXTRA_TEXT, st + " steps")
+            sIntent.putExtra(Intent.EXTRA_SUBJECT, "Look how much have I walked today?!")
             startActivity(Intent.createChooser(sIntent, "Share steps via"))
         }
 
@@ -115,7 +115,7 @@ class HomeFragment : Fragment(), SensorEventListener {
         }
 
         if (running) {
-            step_count.text = event!!.values[0].toString()
+            step_count.text = event!!.values[0].toInt().toString()
             secondaryProgressStatus  = event.values[0].toInt()
 
         }
